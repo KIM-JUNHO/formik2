9dd3dad437333a131788634a1d87b4ec31af5a35
Unknown <prpr48@naver.com>
2020-02-16 16:39:18 +0900

    basic formik, textfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import './App.css';
+import { Formik } from 'formik';
+import { TextField } from '@material-ui/core';
 
-function App() {
-  return <div className="App">Hello formik2</div>;
+export default function App() {
+  return (
+    <div>
+      <Formik
+        initialValues={{ firstName: 'bob' }}
+        onSubmit={data => {
+          alert(data.firstName);
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              name="firstName"
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
 }
-
-export default App;
