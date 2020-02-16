de21eeaba97ad6f9961809ef755ce78b215d226f
Unknown <prpr48@naver.com>
2020-02-16 17:20:17 +0900

    radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
-import { TextField, Button, Checkbox } from '@material-ui/core';
+import { TextField, Button, Checkbox, Radio } from '@material-ui/core';
 
 export default function App() {
   return (
@@ -9,8 +9,9 @@ export default function App() {
         initialValues={{
           firstName: '',
           lastName: '',
-          isTall: '',
-          cookies: []
+          isTall: false,
+          cookies: [],
+          yogurt: ''
         }}
         onSubmit={(data, { setSubmitting }) => {
           setSubmitting(true);
@@ -35,26 +36,25 @@ export default function App() {
                 as={TextField}
               />
             </div>
-            <Field name="isTall" type="checkbox" as={Checkbox}></Field>
+            <Field name="isTall" type="checkbox" as={Checkbox} />
             <div>cookies:</div>
             <Field
               name="cookies"
               type="checkbox"
               value="chocolate chip"
               as={Checkbox}
-            ></Field>
+            />
             <Field
               name="cookies"
               type="checkbox"
               value="snickerdoodle"
               as={Checkbox}
-            ></Field>
-            <Field
-              name="cookies"
-              type="checkbox"
-              value="sugar"
-              as={Checkbox}
-            ></Field>
+            />
+            <Field name="cookies" type="checkbox" value="sugar" as={Checkbox} />
+            <div>yogurt</div>
+            <Field name="yogurt" type="radio" value="peach" as={Radio} />
+            <Field name="yogurt" type="radio" value="blueberry" as={Radio} />
+            <Field name="yogurt" type="radio" value="apple" as={Radio} />
             <div>
               <Button disabled={isSubmitting} type="submit">
                 submit
