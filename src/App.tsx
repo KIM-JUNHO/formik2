2ee931dca5a916e139849e8f974d0d28da75e3b9
Unknown <prpr48@naver.com>
2020-02-16 17:29:50 +0900

    custom radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import { Formik, Field, Form } from 'formik';
-import { TextField, Button, Checkbox, Radio } from '@material-ui/core';
+import { Formik, Field, Form, useField, FieldAttributes } from 'formik';
+import {
+  TextField,
+  Button,
+  Checkbox,
+  Radio,
+  FormControlLabel
+} from '@material-ui/core';
+
+type MyRadioProps = { label: string } & FieldAttributes<{}>;
+
+const MyRadio: React.FC<MyRadioProps> = ({ label, ...props }) => {
+  const [field] = useField<{}>(props);
+  return <FormControlLabel {...field} control={<Radio />} label={label} />;
+};
 
 export default function App() {
   return (
@@ -52,9 +65,14 @@ export default function App() {
             />
             <Field name="cookies" type="checkbox" value="sugar" as={Checkbox} />
             <div>yogurt</div>
-            <Field name="yogurt" type="radio" value="peach" as={Radio} />
-            <Field name="yogurt" type="radio" value="blueberry" as={Radio} />
-            <Field name="yogurt" type="radio" value="apple" as={Radio} />
+            <MyRadio name="yogurt" type="radio" value="peach" label="peach" />
+            <MyRadio
+              name="yogurt"
+              type="radio"
+              value="blueberry"
+              label="blueberry"
+            />
+            <MyRadio name="yogurt" type="radio" value="apple" label="apple" />
             <div>
               <Button disabled={isSubmitting} type="submit">
                 submit
